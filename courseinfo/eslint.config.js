67bf1c894c326c21845b8c83dc4125ed24ae4b14
bjscott77 <bjscott77@gmail.com>
2025-05-20 23:03:10 -0500

    updated eslint config js

--- courseinfo/eslint.config.js
+++ courseinfo/eslint.config.js
@@ -27,7 +27,8 @@ export default [
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
-      ],
+        ],
+      'react/prop-types': 0,
     },
   },
 ]
