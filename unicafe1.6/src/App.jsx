e30bdfd0f61f1569ec2aafc93cd8a470103fb82b
bjscott77 <bjscott77@gmail.com>
2025-05-22 17:24:45 -0500

    Part 1 section 1.7 & 1.8: submission

--- unicafe1.6/src/App.jsx
+++ unicafe1.6/src/App.jsx
@@ -3,15 +3,39 @@ import { useState } from 'react'
 const Header = ({text}) => <h2>{text}</h2>
 const Button = ({name, handleClick}) => <button onClick={handleClick}>{name}</button>
 const DisplayResults = ({type, count}) => <div>{type} {count}</div>
+const Statistics = ({fValue, totalCount, positive}) => {
+    return <div>
+        <DisplayResults type={"all"} count={totalCount} />
+        <DisplayResults type={"average"} count={fValue/totalCount} />
+        <DisplayResults type={"positive"} count={(positive/totalCount)*100+" %"} />
+    </div>
+}
 
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [positive, setPositive] = useState(0)
+    const [total, setTotal] = useState(0)
+    const [runTotal, setRunTotal] = useState(0)
 
-    const onGood = (newVal) => setGood(newVal)
-    const onNeutral = (newVal) => setNeutral(newVal)
-    const onBad = (newVal) => setBad(newVal)
+    const onGood = (newVal) => {
+        setTotal(total+1)
+        setPositive(positive+1)
+        setRunTotal(runTotal+1)
+        setGood(newVal)
+    }
+
+    const onNeutral = (newVal) => {
+        setTotal(total+1)
+        setNeutral(newVal)
+    }
+    
+    const onBad = (newVal) => {
+        setTotal(total+1)
+        setRunTotal(runTotal-1)
+        setBad(newVal)
+    }
 
     return (
         <div>
@@ -22,7 +46,8 @@ const App = () => {
             <Header text={"statistics"} />
             <DisplayResults type={"good"} count={good} />
             <DisplayResults type={"neutral"} count={neutral} />
-            <DisplayResults type={"bad"} count={bad} />
+            <DisplayResults type={"bad"} count={bad} />            
+            <Statistics fValue={runTotal} totalCount={total} positive={good} />
         </div>
     )
 }
