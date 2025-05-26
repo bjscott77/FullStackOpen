39456ea4889b17e1be678b766d3a8a7248debbf9
bjscott77 <bjscott77@gmail.com>
2025-05-26 14:16:19 -0500

    Part 2.9* The Phonebook Step 4 submission

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -2,10 +2,17 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '555-5555' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
+  const [search, setSearch] = useState("")  
+  const filteredPersons = persons.filter(p =>
+    p.name.toLowerCase().includes(search.toLowerCase()) || p.number.includes(search)
+  )
 
   const handleNameChange = (event) => {
       setNewName(event.target.value)
@@ -13,6 +20,10 @@ const App = () => {
 
   const handleNumberChange = (event) => {
       setNewNumber(event.target.value)  
+  }
+
+  const handleSearchChange = (event) => {
+      setSearch(event.target.value)
   }
 
   const handleAddRecord = (name, number) => {
@@ -32,10 +43,14 @@ const App = () => {
             setNewNumber("")
       }
   }
-
+  
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input value={search} onChange={handleSearchChange} placeholder="Search by name or number..." />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={() => handleAddRecord(newName, newNumber)}>
         <div>
           name: <input value={newName} placeholder={"Enter your name..."} onChange={handleNameChange} />
@@ -49,7 +64,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(p => <div key={p.number}>{p.name} {p.number}</div>)}
+        {filteredPersons.map(p => <div key={p.number}>{p.name} {p.number}</div>)}
       </div>
     </div>
   )
